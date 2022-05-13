59a98444b210a45d810ebcb21b122aa093f05406
trax07 <61095643+trax07@users.noreply.github.com>
2022-05-13 15:53:43 +0300

    Delete MineSweeperProject directory

--- MineSweeperProject/Main.java
+++
@@ -1,9 +0,0 @@
-package MineSweeperProject;
-
-public class Main {
-
-    public static void main(String[] args) {
-        MineSweeper mineSweeper = new MineSweeper(4,4);
-        mineSweeper.run();
-    }
-}
