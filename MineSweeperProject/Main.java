95efaf77a02b20ab344c06537cda67e9df1ee637
Barış Yenigün <61095643+barisyenigun07@users.noreply.github.com>
2022-11-22 21:38:29 +0000

    Delete MineSweeperProject directory

--- MineSweeperProject/Main.java
+++
@@ -1,9 +0,0 @@
-package MineSweeperProject;
-
-public class Main {
-
-    public static void main(String[] args) {
-        MineSweeper mineSweeper = new MineSweeper(3,3);
-        mineSweeper.run();
-    }
-}
